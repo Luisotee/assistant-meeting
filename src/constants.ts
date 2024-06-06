12d9f96949fcd6a21b8dd525ffbd14a1c0a65a11
Luis <luisotaviomgr1@gmail.com>
2024-06-05 22:12:01 -0300

    chore: Update constants and dependencies

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,7 @@ import dotenv from "dotenv";
 import dotenvExpand from "dotenv-expand";
 dotenvExpand.expand(dotenv.config());
 
-export const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID as string;
-export const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN as string;
+export const WEBHOOK_VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN as string;
+export const GRAPH_API_TOKEN = process.env.GRAPH_API_TOKEN as string;
+export const PORT = process.env.PORT as string;
+export const NUMBER_ID = process.env.NUMBER_ID as string;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,84 @@
-import twilio from "twilio";
-import { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } from "./constants";
+import axios from "axios";
+import express from "express";
+import { GRAPH_API_TOKEN, PORT, WEBHOOK_VERIFY_TOKEN } from "./constants";
 
-const accountSid = TWILIO_ACCOUNT_SID;
-const authToken = TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
+const app = express();
+app.use(express.json());
 
-client.messages
-  .create({
-    body: "This is a message that I want to send over WhatsApp with Twilio!",
-    from: "whatsapp:+14155238886",
-    to: "whatsapp:+15005550006",
-  })
-  .then((message: any) => console.log(message.sid));
+app.post("/webhook", async (req, res) => {
+  // log incoming messages
+  console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
+
+  // check if the webhook request contains a message
+  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+
+  // check if the incoming message contains text
+  if (message?.type === "text") {
+    // extract the business number to send the reply from it
+    const business_phone_number_id =
+      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+
+    console.log("Business phone number ID:", business_phone_number_id);
+
+    // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
+    await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/v20.0/${business_phone_number_id}/messages`,
+      headers: {
+        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        to: message.from,
+        text: { body: "Echo: " + message.text.body },
+        context: {
+          message_id: message.id, // shows the message as a reply to the original user message
+        },
+      },
+    });
+
+    // mark incoming message as read
+    await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/v20.0/${business_phone_number_id}/messages`,
+      headers: {
+        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        status: "read",
+        message_id: message.id,
+      },
+    });
+  }
+
+  res.sendStatus(200);
+});
+
+// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    // respond with 200 OK and challenge token from the request
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
+  } else {
+    // respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send(`<pre>Nothing to see here.
+  Checkout README.md to start.</pre>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
